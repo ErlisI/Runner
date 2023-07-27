1a037f2af6e1c2f9fd2a83f70453424604c3f3d2
asber <114946775+Asbern3333@users.noreply.github.com>
2023-07-27 13:44:36 -0400

    Update 20230727145112-create-table.js

--- backend/migrations/20230727145112-create-table.js
+++ backend/migrations/20230727145112-create-table.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Tables', {
+    await queryInterface.createTable('Table', {
       id: {
         allowNull: false,
         autoIncrement: true,
