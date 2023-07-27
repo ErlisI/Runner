288568e9d4ce035ceeecac77fbe3ff4bc29394ba
asber <114946775+Asbern3333@users.noreply.github.com>
2023-07-27 14:02:02 -0400

    Update table.js

--- backend/models/table.js
+++ backend/models/table.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsToMany(models.Restaurant),
+      this.belongsTo(models.Restaurant),
       this.hasMany(models.Party_Order),
       
     }
