5585712c6d6e36d7947cc4e27e9da60d6c6fc7bf
Erlis Ibraimi <erloibri1@gmail.com>
2023-08-04 17:54:18 -0400

    fixed the delete request for the tables

--- frontend/src/threesrctors/first.jsx
+++ frontend/src/threesrctors/first.jsx
@@ -4,15 +4,14 @@ import Table from "./Table";
 
 export default function First() {
   const { tables } = useLoaderData();
+  // eslint-disable-next-line no-unused-vars
   const [tableData, setTableData] = useState({});
   const [sortedTables, setSortedTables] = useState([]);
-
   useEffect(() => {
-    // Function to handle the POST request when the button is clicked
+    
     const handleAddTable = () => {
-      // Replace 'your_api_endpoint' with the actual API endpoint URL
       const apiEndpoint = '/api/restaurant/rTables';
-
+    
       fetch(apiEndpoint, {
         method: 'POST',
         headers: {
@@ -27,27 +26,26 @@ export default function First() {
           return response.json();
         })
         .then(data => {
-          // Handle the response from the server (if needed)
           console.log('Response from server:', data);
-
-          // Refresh the page after successful POST request
-          window.location.reload();
+    
+          setSortedTables(prevTables => [...prevTables, data]);
+    
+          setTableData({});
         })
         .catch(error => {
-          // Handle any errors that occurred during the request
           console.error('Error during the POST request:', error);
         });
     };
+    
 
-    // Attach the event listener to the button when the component is mounted
     const addButton = document.getElementById('addTableButton');
     addButton.addEventListener('click', handleAddTable);
 
-    // Cleanup: remove the event listener when the component is unmounted
     return () => {
       addButton.removeEventListener('click', handleAddTable);
     };
-  }, [tableData]); // Make sure to add any dependencies that should trigger re-running this effect
+  }, [tableData]);
+
 
   useEffect(() => {
     // Sort the tables based on their tableNum in ascending order
@@ -55,18 +53,40 @@ export default function First() {
     setSortedTables(sortedTables);
   }, [tables]);
 
+
+  const handleDeleteTable = (tableId) => {
+    const apiEndpoint = `/api/restaurant/rTables/${tableId}`;
+  
+    fetch(apiEndpoint, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        setSortedTables(prevTables => prevTables.filter(table => table.id !== tableId));
+      })
+      .catch(error => {
+        console.error('Error during the DELETE request:', error);
+      });
+  };
+  
+
   const renderTables = sortedTables.map((table) => (
-    <Table table={table} key={table.id} />
+    <Table table={table} key={table.id} onDelete={handleDeleteTable} />
   ));
 
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center py-15 shadow-md shadow-black/5">
-  <div className="flex justify-center items-start flex-grow">
-    <div className="mx-auto mb-auto overflow-y-auto">{renderTables}</div>
-  </div>
-  <button id="addTableButton" className="bg-white hover:bg-red-600 hover:border-red-600 hover:text-white text-red-600 font-bold py-1 px-6 mb-4 rounded-full border border-red-600">
-    Add table
-  </button>
-</div>
+    <div className="flex flex-col h-[80vh] items-center justify-center py-15 shadow-md shadow-black/5">
+      <div className="flex justify-center items-start flex-grow">
+        <div className="mx-auto mb-auto overflow-y-auto">{renderTables}</div>
+      </div>
+      <button id="addTableButton" className="bg-white hover:bg-red-600 hover:border-red-600 hover:text-white text-red-600 font-bold py-1 px-6 mb-4 rounded-full border border-red-600">
+        Add table
+      </button>
+    </div>
   );
 }
