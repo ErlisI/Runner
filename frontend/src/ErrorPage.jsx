0b89d91dbbeca3f5a7071c4a4e3c305f0a737a2b
Erlis Ibraimi <erloibri1@gmail.com>
2023-08-06 01:34:00 -0400

    fixed adding orders

--- frontend/src/ErrorPage.jsx
+++ frontend/src/ErrorPage.jsx
@@ -8,7 +8,7 @@ export default function ErrorPage() {
     <div className="h-screen bg-cover bg-no-repeat bg-[url('/src/assets/desert.jpg')] text-center" id="error-page">
       <img className="h-screen" src="https://media.tenor.com/f3irLBUB6YIAAAAC/eighty-six-86.gif" />
       {/* <h1 className="text-5xl">I Know You Are Looking For Water, But This Is A Desert</h1> */}
-      <h1 className="text-5xl">You were not supposed to see this, I'm sorry! (GG)</h1>
+      <h1 className="text-5xl">You were not supposed to see this, Im sorry! (GG)</h1>
       <p>(404 This Is Not Somewhere You Should Be...)</p>
       
       <p>
